748b46ee808f828319ebaf8d8c4b4c9b36b4a9c3
dev1d123 <dhuamanio@unsa.edu.pe>
2026-03-27 01:38:57 -0500

    fear: add npc attributes

--- lags-2026/game/player/npc.gd
+++ lags-2026/game/player/npc.gd
@@ -1,9 +1,18 @@
 # NPC.gd
 extends CharacterBody2D
+enum TipoNPC {
+	ABUELA,
+	ABUELO,
+	JOVEN,
+	NINO,
+	MUJER
+}
+var tipo: TipoNPC
+var lugar: String 
 
 var mis_animaciones: SpriteFrames
-const speed = 100
-
+const speed = 300
+const STOP_DISTANCE = 12.0
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var nav_agent: NavigationAgent2D = $NavigationAgent2D
 
@@ -19,16 +28,24 @@ func _ready() -> void:
 	await get_tree().physics_frame
 	make_path()
 
-
-
-
-
 var last_position: Vector2
 var stuck_timer: float = 0.0
 const STUCK_THRESHOLD = 1.5  # segundos parado
 const STUCK_MIN_DIST = 2.0   # píxeles mínimos para considerar que se movió
 
 func _physics_process(delta: float) -> void:
+	if pos == null:
+		return
+
+	var dist_to_target = global_position.distance_to(pos.global_position)
+
+	if dist_to_target <= STOP_DISTANCE:
+		velocity = Vector2.ZERO
+		move_and_slide()
+
+		if sprite.animation != "idle":
+			sprite.play("idle")
+		return
 	if nav_agent.is_navigation_finished():
 		velocity = Vector2.ZERO
 		move_and_slide()
@@ -47,9 +64,25 @@ func _physics_process(delta: float) -> void:
 	velocity = dir * speed
 	move_and_slide()
 
+	if velocity.length() < 1:
+		if sprite.animation != "idle":
+			sprite.play("idle")
+	else:
+		if(abs(velocity.x) > abs(velocity.y)):
+			if sprite.animation != "walk_side":
+				sprite.play("walk_side")
+			sprite.flip_h = velocity.x > 0
+		else:
+			if velocity.y < 0:
+				if sprite.animation != "walk_up":
+					sprite.play("walk_up")
+			else:
+				if sprite.animation != "walk_side":
+					sprite.play("walk_side")
+
+
 func make_path() -> void:
 	if pos == null:
-		print("ERROR: player no asignado")
 		return
 	nav_agent.target_position = pos.global_position
 
--- lags-2026/levels/scenario.gd
+++ lags-2026/levels/scenario.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 const NPC_SCENE = preload("res://game/player/NPC.tscn")
+
 @export var pos: Node2D
+
 var sprite_paths: Array[String] = [
 	"res://game/enemies/abuela1.tres", "res://game/enemies/abuela2.tres", "res://game/enemies/abuela3.tres",
 	"res://game/enemies/abuelo1.tres", "res://game/enemies/abuelo2.tres", "res://game/enemies/abuelo3.tres",
@@ -9,14 +11,53 @@ var sprite_paths: Array[String] = [
 	"res://game/enemies/mujer1.tres", "res://game/enemies/mujer2.tres", "res://game/enemies/mujer3.tres",
 	"res://game/enemies/niño1.tres", "res://game/enemies/niño2.tres", "res://game/enemies/niño3.tres"
 ]
+func get_random_marker_by_tipo(tipo: int) -> Node2D:
+	var lugares = []
 
-var npc_data: Dictionary = {}
+	match tipo:
+		0: # ABUELA
+			lugares = [$restaurant, $shop]
+		1: # ABUELO
+			lugares = [$bar, $store]
+		2: # JOVEN
+			lugares = [$cafe, $bar, $store]
+		3: # NINO
+			lugares = [$cafe]
+		4: # MUJER
+			lugares = [$shop, $restaurant]
 
+	var markers: Array[Node2D] = []
+
+	for lugar in lugares:
+		for child in lugar.get_children():
+			if child is Marker2D:
+				markers.append(child)
+
+	if markers.is_empty():
+		print("No hay markers")
+		return null
+
+	return markers.pick_random()
+	
+	
+func get_tipo_from_path(path: String) -> int:
+	if path.contains("abuela"):
+		return 0
+	elif path.contains("abuelo"):
+		return 1
+	elif path.contains("joven"):
+		return 2
+	elif path.contains("niño"):
+		return 3
+	elif path.contains("mujer"):
+		return 4
+	
+	return 2
 func _ready() -> void:
 
 	var timer = Timer.new()
 	add_child(timer)
-	timer.wait_time = 5.0
+	timer.wait_time = 1.0
 	timer.timeout.connect(_spawn_npc)
 	timer.start()
 	_spawn_npc()
@@ -26,7 +67,19 @@ func _spawn_npc() -> void:
 	npc.scale = Vector2(1.72, 1.72)
 	var random_sprite = sprite_paths.pick_random()
 	npc.mis_animaciones = load(random_sprite)
-	npc.pos = pos 
-	add_child(npc)
 
+	# 🔴 definir tipo
+	var tipo = get_tipo_from_path(random_sprite)
+	npc.tipo = tipo
+
+	# 🔴 definir destino según tipo
+	var random_marker = get_random_marker_by_tipo(tipo)
+	npc.pos = random_marker
+
+	# 🔴 guardar nombre del lugar
+	if random_marker != null:
+		npc.lugar = random_marker.get_parent().name
+	print("NPC tipo:", npc.tipo, "-> va a:", npc.lugar)
+	add_child(npc)
+	move_child(npc, 1)
 	npc.global_position = $Marker2DEnter.global_position
